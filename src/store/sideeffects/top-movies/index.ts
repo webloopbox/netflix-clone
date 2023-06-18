9b95906a3f9fcf11ab2450d86d139fff849f445e
Pawel <marekpawel22@wp.pl>
2023-06-18 18:50:38 +0200

    add types

--- src/store/sideeffects/top-movies/index.ts
+++ src/store/sideeffects/top-movies/index.ts
@@ -2,13 +2,16 @@ import { call, put, takeLatest } from "redux-saga/effects";
 import { getGenres } from "../../../api/genres";
 import { getTopMovies } from "../../../api/movies";
 import { movieActions } from "../../movieSlice";
+import { Genre } from "models/Genres";
+import { TopMovieDataResponse } from "models/Movies";
 
-function* fetchTopMovies(): any {
+function* fetchTopMovies() {
   try {
-    const genresData = yield call(getGenres);
-    const topMovieData = yield call(getTopMovies);
+    const genresData: Genre[] = yield call(getGenres);
+    const topMovieData: TopMovieDataResponse = yield call(getTopMovies);
 
     yield put(movieActions.fetchTopMoviesSuccess({ genresData, topMovieData }));
+
   } catch (error) {
     yield put(movieActions.fetchFailure((error as Error).message));
   }
